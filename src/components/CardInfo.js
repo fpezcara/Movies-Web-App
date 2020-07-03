834a6b7db52528508fcd0bd61c73cdffc7740a23
Florencia Pezcara <fpezcara@gmail.com>
2020-07-03 16:05:35 +0100

    empece CardInfo

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -6,6 +6,22 @@ import useFetch from "../hooks/useFetch";
 const Container = styled.article`
   display: flex;
   flex-direction: column;
+  width: 100%;
+`;
+
+const CardImage = styled.article`
+  width: 400px;
+  height: 500px;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const CardBody = styled.article`
+  width: 100%;
+  height: 100%;
 `;
 
 const CardInfo = () => {
@@ -17,17 +33,24 @@ const CardInfo = () => {
     `https://api.themoviedb.org/3/${type}/${id}?api_key=${apiKey}`
   );
 
-  console.log(infoId);
+  console.log(infoId.title);
 
   return (
     <Container>
-      {infoId && (
-        <>
-          <h1>CARD INFO</h1>
-          <p>Nombre: {infoId.title || infoId.name}</p>
-          <p>Rating: {infoId.vote_average} </p>
-        </>
-      )}
+      {/* {infoId && (
+        <> */}
+      <CardImage>
+        <img
+          alt={infoId.title}
+          src={`https://image.tmdb.org/t/p/w500${infoId.poster_path}`}
+        />
+      </CardImage>
+      <CardBody>
+        <h2>{infoId.title || infoId.name}</h2>
+        <p>Rating: {infoId.vote_average} </p>
+      </CardBody>
+      {/* </> */}
+      {/* )} */}
     </Container>
   );
 };
